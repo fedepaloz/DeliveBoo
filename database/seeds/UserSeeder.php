76371185ffd4a84baae7bd9d5e948af32ca5c3cc
marcocalligaris <marcocally@gmail.com>
2022-10-18 18:17:53 +0200

    feat: user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@deliveboo.it';
+        $user->password = bcrypt('admin');
+        $user->save();
+
+        // $user_ids = User::pluck('id')->toArray(); //recupero gli utenti
+        for ($i = 0; $i < 9; $i++) {
+            $user = new User();
+            $user->name = $faker->userName();
+            $user->email = $faker->email();
+            $user->password = bcrypt('password');
+            $user->save();
+        }
+    }
+}
