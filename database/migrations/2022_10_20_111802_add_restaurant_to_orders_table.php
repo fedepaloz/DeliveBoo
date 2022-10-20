6c07ab254ae48e12c4582936eaa6bd9796118777
marcocalligaris <marcocally@gmail.com>
2022-10-20 11:34:51 +0200

    feat: orders restaurants relationship

--- database/migrations/2022_10_20_111802_add_restaurant_to_orders_table.php
+++ database/migrations/2022_10_20_111802_add_restaurant_to_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRestaurantToItemsTable extends Migration
+class AddRestaurantToOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddRestaurantToItemsTable extends Migration
      */
     public function up()
     {
-        Schema::table('items', function (Blueprint $table) {
+        Schema::table('orders', function (Blueprint $table) {
             $table->unsignedBigInteger('restaurant_id')->nullable()->after('id');
             $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('set null');
         });
@@ -26,8 +26,8 @@ class AddRestaurantToItemsTable extends Migration
      */
     public function down()
     {
-        Schema::table('items', function (Blueprint $table) {
-            $table->dropForeign('items_restaurant_id_foreign');
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_restaurant_id_foreign');
             $table->dropColumn('restaurant_id');
         });
     }
