b8a54dd1fb40578cb0ac708e73885073699c3608
SaBobic <fabrizioettori@gmail.com>
2022-10-24 18:17:12 +0200

    added restaurant form front-end validation

---
+++ resources/js/res-form-validation.js
@@ -0,0 +1,110 @@
+const formField = document.getElementById("restaurant-form");
+
+const nameField = document.getElementById("name");
+const nameFeedbackField = document.getElementById("name-feedback");
+const categoryField = document.getElementById("category_id");
+const categoryFeedbackField = document.getElementById("category-feedback");
+const vatField = document.getElementById("vat_number");
+const vatFeedbackField = document.getElementById("vat-feedback");
+const phoneField = document.getElementById("phone");
+const phoneFeedbackField = document.getElementById("phone-feedback");
+const addressField = document.getElementById("address");
+const addressFeedbackField = document.getElementById("address-feedback");
+const openingField = document.getElementById("opening_time");
+const openingFeedbackField = document.getElementById("opening-feedback");
+const closureField = document.getElementById("closure_time");
+const closureFeedbackField = document.getElementById("closure-feedback");
+const minField = document.getElementById("min_order");
+const minFeedbackField = document.getElementById("min-feedback");
+const dcostField = document.getElementById("delivery_cost");
+const dcostFeedbackField = document.getElementById("dcost-feedback");
+
+formField.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let isNameValid = false;
+    let isCategoryValid = false;
+    let isVatValid = false;
+    let isPhoneValid = false;
+    let isAddressValid = false;
+    let isOpeningValid = false;
+    let isClosureValid = false;
+    let isMinValid = false;
+    let isDcostValid = false;
+
+    if (nameField.value) {
+        isNameValid = true;
+        nameField.classList.remove("is-invalid");
+    } else {
+        nameField.classList.add("is-invalid");
+    }
+
+    if (categoryField.value) {
+        isCategoryValid = true;
+        categoryField.classList.remove("is-invalid");
+    } else {
+        categoryField.classList.add("is-invalid");
+    }
+
+    if (vatField.value) {
+        isVatValid = true;
+        vatField.classList.remove("is-invalid");
+    } else {
+        vatField.classList.add("is-invalid");
+    }
+
+    if (phoneField.value) {
+        isPhoneValid = true;
+        phoneField.classList.remove("is-invalid");
+    } else {
+        phoneField.classList.add("is-invalid");
+    }
+
+    if (addressField.value) {
+        isAddressValid = true;
+        addressField.classList.remove("is-invalid");
+    } else {
+        addressField.classList.add("is-invalid");
+    }
+
+    if (openingField.value) {
+        isOpeningValid = true;
+        openingField.classList.remove("is-invalid");
+    } else {
+        openingField.classList.add("is-invalid");
+    }
+
+    if (closureField.value) {
+        isClosureValid = true;
+        closureField.classList.remove("is-invalid");
+    } else {
+        closureField.classList.add("is-invalid");
+    }
+
+    if (minField.value) {
+        isMinValid = true;
+        minField.classList.remove("is-invalid");
+    } else {
+        minField.classList.add("is-invalid");
+    }
+
+    if (dcostField.value) {
+        isDcostValid = true;
+        dcostField.classList.remove("is-invalid");
+    } else {
+        dcostField.classList.add("is-invalid");
+    }
+
+    if (
+        isNameValid &&
+        isCategoryValid &&
+        isVatValid &&
+        isPhoneValid &&
+        isAddressValid &&
+        isOpeningValid &&
+        isClosureValid &&
+        isMinValid &&
+        isDcostValid
+    )
+        formField.submit();
+});
