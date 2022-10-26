dc19ad08b356aa09a25c82f039b6caf1eba7550a
feddericahu99 <federica.hu339@gmail.com>
2022-10-26 23:22:43 +0200

    changed color in footer and fixed a in partners

--- resources/js/components/AppContacts.vue
+++ resources/js/components/AppContacts.vue
@@ -2,16 +2,16 @@
     <section id="become-partner" class="my-5">
         <div class="row align-items-center justify-content-center justify-content-between">
 
-            <div class="col-s-12 col-sm-8 col-l-9 col-10">
+            <div class="col-xs-12 col-sm-8 col-l-9 col-10">
                 <h1 class="text-success"> Diventa nostro Partner</h1>
-                <a><i class="fa-solid fa-phone text-success"></i> 092 456 444</a>
-                <a><i class="fa-solid fa-envelope text-success"></i>  Deliveboo@food.it</a>
-                <a><i class="fa-brands fa-square-facebook text-success"></i> DeliveBoo</a>
-                <a><i class="fa-brands fa-twitter text-success"></i> DeliveBoo</a>
-                <a><i class="fa-brands fa-instagram text-success"></i> DeliveBoo</a>
+                <a href="#"> <i class="fa-solid fa-phone text-success"></i> 092 456 444</a>
+                <a href="#"> <i class="fa-solid fa-envelope text-success"></i>  Deliveboo@food.it</a>
+                <a href="#"> <i class="fa-brands fa-square-facebook text-success"></i> DeliveBoo</a>
+                <a href="#"> <i class="fa-brands fa-twitter text-success"></i> DeliveBoo</a>
+                <a href="#"> <i class="fa-brands fa-instagram text-success"></i> DeliveBoo</a>
             </div>
 
-            <div class="col-s-12 col-sm-4 col-l-3 col-2">
+            <div class="col-xs-12 col-sm-4 col-l-3 col-2">
                 <img class="img-fluid" src="https://static.vecteezy.com/system/resources/previews/005/548/794/original/3-green-people-or-human-teamwork-logo-symbol-icon-graphic-design-illustration-vector.jpg" alt="Partnership"/>
             </div>
 
@@ -37,6 +37,7 @@ a {
     display:block;
     margin: 5px;
     font-size: 20px;
-    text-align: center
+    text-align: center;
+    color: black;
 };
 </style>
