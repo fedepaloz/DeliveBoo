235ae2208c3c5210592649f02a1d2d8acc98c713
feddericahu99 <federica.hu339@gmail.com>
2022-10-26 21:38:45 +0200

    style homepage

---
+++ resources/js/components/AppServices.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1 class="font-weight-bold">I nostri servizi</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam deserunt,
+        eaque iure illum qui iste quaerat voluptates molestias alias veniam
+        beatae exercitationem suscipit. Perferendis sit quos optio ratione
+        laboriosam aspernatur? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Incidunt provident ex.
+      </p>
+      <!-- Rivedere rotta (about) -->
+      <a class="text-success" href=""> Scopri di più su di noi</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"AppServices"
+};
+</script>
+
+<style scoped>
+a {
+    border: 1.5px solid lightgreen;
+    padding: 5px 3px;
+}
+</style>
