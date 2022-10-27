87ce4c8956688107427935b4288574de1efd396a
virtu-alex <alessio.cordari@gmail.com>
2022-10-27 10:52:15 +0200

    fix

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from './components/pages/HomePage.vue'
+import RestaurantPage from './components/pages/RestaurantPage.vue'
 
 Vue.use(VueRouter)
 
@@ -8,8 +9,8 @@ const routes = new VueRouter({
     mode: 'history',
     // linkExactActiveClass: 'active',
     routes: [
-        { path: '/restaurants', component: HomePage, name: 'home' },
-        // { path: '/about', component: AboutPage, name: 'about' },
+        { path: '/', component: HomePage, name: 'home' },
+        { path: '/restaurants/:id', component: RestaurantPage, name: 'restaurant-page' },
         // { path: '/contacts', component: ContactsPage, name: 'contacts' },
         // { path: '/posts/:slug', component: PostDetailPage, name: 'post-detail' },
         // { path: '*', component: NotFoundPage, name: 'not_found' },
