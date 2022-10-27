6d93704e2a9a86b1af8f6dfeded8c2b8d4213106
SaBobic <fabrizioettori@gmail.com>
2022-10-27 11:10:09 +0200

    added restaurantpage layout

--- resources/js/components/pages/RestaurantPage.vue
+++ resources/js/components/pages/RestaurantPage.vue
@@ -1,50 +1,41 @@
 <template lang="">
     <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <h1>Nome ristorante</h1>
-                <ul class="list-group">
-                    <li class="list-group-item disabled" aria-disabled="true">
-                        <div class="card mb-3">
-                            <img src="" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h4 class="card-title">Nome Piatto: ...</h4>
-                                <p class="card-text">descrizione lunga piattooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo.</p>
-                                <h5 class="card-text">Prezzo: ....</h5>
-                                <!-- !!! Bottone da rivedere !!! -->
-                                <button>Acquista</button>
-                                <ul class="list-group">
-                                    <!-- ciclo da fare qui -->
-                                    <li class="list-group-item">Primo prodotto</li>
-                                </ul>
-                                <!-- Da rivedere -->
-                                <div class="row">
-                                    <div class="col-6">
-                                        <h3>Totale: ...</h3>
-                                    </div>
-                                    <div class="col-6">
-                                        <button class="btn btn-success">Acquista:</button>
-                                    </div>
-                                </div>
-                                
-                                
-                          </div>
-                        </div>
-                    </li>
-                </ul>
-
+        <div class="container">
+            <!-- Dettagli ristorante -->
+            <div class="row">
+                <div class="col-12 py-4">
+                    <!-- ! Qui inserire componente dettagli ristorante -->
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Porro explicabo eligendi cumque impedit? Quae deleniti
+                    tempore soluta harum, dolorum molestias? Odio, fugit
+                    doloribus similique autem a nesciunt in consequatur nostrum.
+                </div>
             </div>
-            <div class="col-4">
-                <h3>Carrello:</h3>
-                <ul></ul>
+
+            <div class="row">
+                <!-- Menu ristorante -->
+                <div class="col-8">
+                    <!-- ! Qui inserire componente menu -->
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Porro explicabo eligendi cumque impedit? Quae deleniti
+                    tempore soluta harum, dolorum molestias? Odio, fugit
+                    doloribus similique autem a nesciunt in consequatur nostrum.
+                </div>
+                <!-- Carrello sticky -->
+                <div class="col-4">
+                    <!-- ! Qui inserire componente carrello -->
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Porro explicabo eligendi cumque impedit? Quae deleniti
+                    tempore soluta harum, dolorum molestias? Odio, fugit
+                    doloribus similique autem a nesciunt in consequatur nostrum.
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
 export default {
-  name: "RestaurantPage",
+    name: "RestaurantPage",
 };
 </script>
-<style lang="">
-</style>
+<style lang="scss" scoped></style>
