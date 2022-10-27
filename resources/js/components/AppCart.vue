88760f011522b2e86cafd349dcc53ca1ee17c54a
fedepaloz <palozzifederico@gmail.com>
2022-10-27 16:31:07 +0200

    fix cart

--- resources/js/components/AppCart.vue
+++ resources/js/components/AppCart.vue
@@ -1,75 +1,64 @@
 <template>
-  
-    <div class="h-100 py-5">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-10">
-          <div class="d-flex justify-content-between align-items-center mb-4">
-            <h3 class="fw-normal mb-0 text-black">Carrello</h3>
-          </div>
+  <div class="py-5">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col">
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <h3 class="fw-normal mb-0 text-black">Carrello</h3>
+        </div>
 
-          <div class="card rounded-3 mb-4">
-            <div class="card-body">
-              <div
-                class="row d-flex justify-content-between align-items-center"
-              >
-                <div class="col-md-2 col-lg-2 col-xl-2">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                    class="img-fluid rounded-3"
-                    alt="Cotton T-shirt"
-                  />
-                </div>
-                <div class="col-md-3 col-lg-3 col-xl-3">
-                  <p class="lead fw-normal mb-2">piatto</p>
-                  
-                </div>
-                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                  <button
-                    class="btn btn-link px-2"
-                   
-                  >
-                    <i class="fas fa-minus"></i>
-                  </button>
+        <div class="card rounded-3 mb-4">
+          <div class="card-body">
+            <div class="row d-flex justify-content-between align-items-center">
+              <div class="col-md-2 col-lg-2 col-xl-2">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
+                  class="img-fluid rounded-3"
+                  alt="Cotton T-shirt"
+                />
+              </div>
+              <div class="col-md-3 col-lg-3 col-xl-3">
+                <p class="lead fw-normal mb-2">kebab lungo un metro</p>
+              </div>
+              <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                <button class="btn btn-link px-2">
+                  <i class="fas fa-minus"></i>
+                </button>
 
-                  <input
-                    id="form1"
-                    min="0"
-                    name="quantity"
-                    value="2"
-                    type="number"
-                    class="form-control form-control-sm"
-                  />
+                <input
+                  id="form1"
+                  min="0"
+                  name="quantity"
+                  value="2"
+                  type="number"
+                  class="form-control form-control-sm"
+                />
 
-                  <button
-                    class="btn btn-link px-2"
-                    
-                  >
-                    <i class="fas fa-plus"></i>
-                  </button>
-                </div>
-                <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                  <h5 class="mb-0">25$</h5>
-                </div>
-                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                  <a href="#!" class="text-danger"
-                    ><i class="fas fa-trash fa-lg"></i
-                  ></a>
-                </div>
+                <button class="btn btn-link px-2">
+                  <i class="fas fa-plus"></i>
+                </button>
+              </div>
+              <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                <h5 class="mb-0">25$</h5>
+              </div>
+              <div class="col-md-1 col-lg-1 col-xl-1 mr-1">
+                <a href="#!" class="text-danger"
+                  ><i class="fas fa-trash fa-lg"></i
+                ></a>
               </div>
             </div>
           </div>
+        </div>
 
-          <div class="card">
-            <div class="card-body">
-              <button type="button" class="btn btn-warning btn-block btn-lg">
-                Procedi al pagamento
-              </button>
-            </div>
+        <div class="card">
+          <div class="card-body">
+            <button type="button" class="btn btn-success btn-block btn-lg">
+              Procedi al pagamento
+            </button>
           </div>
         </div>
       </div>
     </div>
-  
+  </div>
 </template>
 
 <script>
@@ -79,5 +68,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 </style>
 
