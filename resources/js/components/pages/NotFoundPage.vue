85f39efe1040f5751a3e75b4d801c4bd610308d2
feddericahu99 <federica.hu339@gmail.com>
2022-10-28 19:38:12 +0200

    notfoundpage

---
+++ resources/js/components/pages/NotFoundPage.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container" id="not-found">
+        <div id="not-fount-content">
+            <div class="d-flex justify-content-center align-items-center"><h1>404 | Not found</h1></div>
+            <div class="d-flex justify-content-center align-items-center"><a href="\">Torna nella Home</a></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NotFoundPage"
+}
+</script>
+
+<style scoped lang="scss" >
+#not-found {
+    height:90vh;
+}
+
+#not-fount-content {
+    margin: 30vh 0 5vh 0;
+}
+
+a {
+    border: 3px solid lightgreen;
+    padding: 8px 8px;
+    border-radius: 24px;
+    font-size: 17px;
+    font-weight: bold;
+    transition: 0.6s;
+    color: green;
+    text-decoration: none;
+
+    &:hover{
+        font-size: 18px;
+        background-color: #60c442;
+        color:white;
+    }
+}
+</style>
--- resources/js/router.js
+++ resources/js/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomePage from './components/pages/HomePage.vue'
 import RestaurantPage from './components/pages/RestaurantPage.vue'
+import AboutPage from './components/pages/AboutPage.vue'
+import NotFoundPage from './components/pages/NotFoundPage.vue'
 
 Vue.use(VueRouter)
 
@@ -13,7 +15,8 @@ const routes = new VueRouter({
         { path: '/restaurants/:id', component: RestaurantPage, name: 'restaurant-page' },
         // { path: '/contacts', component: ContactsPage, name: 'contacts' },
         // { path: '/posts/:slug', component: PostDetailPage, name: 'post-detail' },
-        // { path: '*', component: NotFoundPage, name: 'not_found' },
+        { path: '*', component: NotFoundPage, name: 'not-found' },
+        { path: '/about', component: AboutPage, name: 'about' },
     ]
 });
 
