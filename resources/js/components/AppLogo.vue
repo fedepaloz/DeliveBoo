f2ef9a7932e8d4d04c02eee26ab5aac8d6426000
feddericahu99 <federica.hu339@gmail.com>
2022-10-28 19:37:49 +0200

    app logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="justify-content-center">
-        <img class="img-fluid" src="" alt="Deliveboo logo">
+    <div class="d-flex justify-content-center">
+        <img class="img-fluid" src="https://cdn-icons-png.flaticon.com/512/5503/5503207.png" alt="Deliveboo logo">
     </div>
 </template>
 <script>
