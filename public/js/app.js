961e1564de44aad7cfba70ef3c20a211d61aaecb
fedepaloz <palozzifederico@gmail.com>
2022-11-02 15:46:47 +0100

    prova

--- public/js/app.js
+++ public/js/app.js
@@ -37304,6 +37304,8 @@ module.exports = function(module) {
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 // require('./image_preview');
 
+'bella zi0';
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
