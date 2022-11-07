85425ed9b48aa43595bc2e8361d6b5213d1d4792
SaBobic <fabrizioettori@gmail.com>
2022-11-07 16:07:46 +0100

    changed login route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/admin';
+    public const HOME = '/admin/restaurants';
     public const CREATE = '/admin/restaurants/create';
 
     /**
