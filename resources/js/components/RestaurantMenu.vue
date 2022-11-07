ea9ad55aa8fdb537f3a797a401dae93eaaca801b
virtu-alex <alessio.cordari@gmail.com>
2022-11-07 16:37:57 +0100

    fix-not-working

--- resources/js/components/RestaurantMenu.vue
+++ resources/js/components/RestaurantMenu.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <ul class="list-unstyled">
       <li
@@ -31,47 +30,6 @@
       </li>
     </ul>
   </div>
-=======
-    <div>
-        <ul class="list-unstyled">
-            <li
-                v-for="item in items"
-                :key="item.id"
-                class="item-card col-12 mb-3 border border-dark rounded"
-            >
-                <div v-if="item.visible">
-                    <div class="row">
-                        <div class="col-3">
-                            <img
-                                :src="item.image"
-                                alt=""
-                                class="img-fluid py-2"
-                            />
-                        </div>
-                        <div class="col-9 py-2 d-flex flex-column">
-                            <div>
-                                <h3>{{ item.name }}</h3>
-                                <div class="mb-1">{{ item.description }}</div>
-                            </div>
-                            <div class="h5 mb-0">{{ item.price }} €</div>
-                        </div>
-                        <div class="col-9 offset-3 mb-2">
-                            <span
-                                v-if="item.visible"
-                                class="btn btn-outline-primary btn-sm"
-                                @click="addToCart(item)"
-                                >Aggiungi al carrello
-                            </span>
-                            <span v-else class="text-danger"
-                                >Prodotto non disponibile
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        </ul>
-    </div>
->>>>>>> c95c4483303fd456a7844e1ece27c4dff8e11fd5
 </template>
 
 <script>
